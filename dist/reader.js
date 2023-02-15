7dfbf7cb4aa83438562ae63bd5790e56b5df77f1
xulihang <xulihanghai@163.com>
2023-02-15 13:51:25 +0800

    add a modal to show the decoding results

--- dist/reader.js
+++ dist/reader.js
@@ -1,10 +1,52 @@
-let reader;
+let DBRReader;
 async function decode(img) {
-  if (!reader) {
-    reader = await Dynamsoft.DBR.BarcodeReader.createInstance();
+  showModal();
+  if (!DBRReader) {
+    updateStatus("Initializing...");
+    DBRReader = await Dynamsoft.DBR.BarcodeReader.createInstance();
   }
-  const results = await reader.decode(img);
-  alert("Found "+results.length+" barcode(s).");
+  updateStatus("Decoding...");
+  const results = await DBRReader.decode(img);
+  updateStatus("Found "+results.length+((results.length>1)?" barcodes.":" barcode."));
+  if (results.length > 0) {
+    const resultList = document.querySelector(".dbr-results");
+    resultList.innerHTML = "";
+    for (let index = 0; index < results.length; index++) {
+      const result = results[index];
+      const item = document.createElement("li");
+      item.innerText = result.barcodeFormatString + ": " + result.barcodeText;
+      resultList.appendChild(item);
+    }
+  }
+}
+
+function updateStatus(text){
+  let status = document.querySelector(".dbr-status");
+  if (status) {
+    status.innerText = text;
+  }
+}
+
+function showModal(){
+  let modal = document.createElement("div");
+  modal.className = "dbr-modal";
+  let status = document.createElement("div");
+  status.className = "dbr-status";
+  let resultList = document.createElement("ol");
+  resultList.className = "dbr-results";
+  let closeBtn = document.createElement("button");
+  closeBtn.innerText = "Close";
+  closeBtn.addEventListener("click", () => {
+    removeModal();
+  });
+  modal.appendChild(status);
+  modal.appendChild(resultList);
+  modal.appendChild(closeBtn);
+  document.body.appendChild(modal);
+}
+
+function removeModal(){
+  document.querySelector(".dbr-modal").remove();
 }
 
 function decodeFromSrc() {
