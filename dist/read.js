957f7bce945194d2f103e284332fc2717c6bda24
xulihang <xulihanghai@163.com>
2023-02-17 15:50:20 +0800

    add namespace protection

--- dist/read.js
+++ dist/read.js
@@ -1,9 +1,8 @@
-console.log("read");
 (()=>{
   const dbrEle = document.getElementById("dbr")
   const src = dbrEle.getAttribute("imgSrc");
   const template = dbrEle.getAttribute("template");
-  decodeFromSrc(src,template);
+  DBRChromeExtension.decodeFromSrc(src,template);
 })();
 
 
--- dist/reader.js
+++ dist/reader.js
@@ -1,88 +1,84 @@
-let DBRReader;
-let previousDBRTemplate = "";
-
-async function updateRuntimeSettings(template){
-  if (template != undefined) {
-    if (previousDBRTemplate != template) {
-      if (template === "") {
-        await DBRReader.resetRuntimeSettings();
-      }else{
-        await DBRReader.initRuntimeSettingsWithString(template);
+let DBRChromeExtension = {
+  DBRReader:undefined,
+  previousDBRTemplate:"",
+  updateRuntimeSettings: async function(template) {
+    if (template != undefined) {
+      if (this.previousDBRTemplate != template) {
+        if (template === "") {
+          await this.DBRReader.resetRuntimeSettings();
+        }else{
+          await this.DBRReader.initRuntimeSettingsWithString(template);
+        }
+        this.previousDBRTemplate = template;
       }
-      previousDBRTemplate = template;
     }
-  }
-}
-
-async function decode(img, template) {
-  showModal();
-  if (!DBRReader) {
-    updateStatus("Initializing...");
-    DBRReader = await Dynamsoft.DBR.BarcodeReader.createInstance();
-  }
-  updateRuntimeSettings(template);
-  updateStatus("Decoding...");
-  let results;
-  try {
-    results = await DBRReader.decode(img);  
-  } catch (error) {
-    updateStatus("Error "+error);
-    return;
-  }
-  updateStatus("Found "+results.length+((results.length>1)?" barcodes.":" barcode."));
-  if (results.length > 0) {
-    const resultList = document.querySelector(".dbr-results");
-    resultList.innerHTML = "";
-    for (let index = 0; index < results.length; index++) {
-      const result = results[index];
-      const item = document.createElement("li");
-      item.innerText = result.barcodeFormatString + ": " + result.barcodeText;
-      resultList.appendChild(item);
+  },
+  decode:async function(img, template) {
+    this.showModal();
+    if (!this.DBRReader) {
+      this.updateStatus("Initializing...");
+      this.DBRReader = await Dynamsoft.DBR.BarcodeReader.createInstance();
     }
-  }
-}
-
-function updateStatus(text){
-  let status = document.querySelector(".dbr-status");
-  if (status) {
-    status.innerText = text;
-  }
-}
-
-function showModal(){
-  let modal = document.createElement("div");
-  modal.className = "dbr-modal";
-  let status = document.createElement("div");
-  status.className = "dbr-status";
-  let resultList = document.createElement("ol");
-  resultList.className = "dbr-results";
-  let closeBtn = document.createElement("button");
-  closeBtn.innerText = "Close";
-  closeBtn.addEventListener("click", () => {
-    removeModal();
-  });
-  modal.appendChild(status);
-  modal.appendChild(resultList);
-  modal.appendChild(closeBtn);
-  document.body.appendChild(modal);
-}
-
-function removeModal(){
-  document.querySelector(".dbr-modal").remove();
-}
-
-function decodeFromSrc(src, template) {
-  if (src) {
-    let img = document.createElement("img");
-    img.crossOrigin = "Anonymous";
-    img.src = src;  
-    img.onerror = function (e) {
-      showModal();
-      updateStatus("Unable to load the image");
+    this.updateRuntimeSettings(template);
+    this.updateStatus("Decoding...");
+    let results;
+    try {
+      results = await this.DBRReader.decode(img);  
+    } catch (error) {
+      this.updateStatus("Error "+error);
+      return;
     }
-    img.onload = async function() {
-      await decode(img, template);
-      document.getElementById("dbr").remove();
+    this.updateStatus("Found "+results.length+((results.length>1)?" barcodes.":" barcode."));
+    if (results.length > 0) {
+      const resultList = document.querySelector(".dbr-results");
+      resultList.innerHTML = "";
+      for (let index = 0; index < results.length; index++) {
+        const result = results[index];
+        const item = document.createElement("li");
+        item.innerText = result.barcodeFormatString + ": " + result.barcodeText;
+        resultList.appendChild(item);
+      }
+    }
+  },
+  updateStatus: function(text) {
+    let status = document.querySelector(".dbr-status");
+    if (status) {
+      status.innerText = text;
+    }
+  },
+  showModal: function(){
+    let modal = document.createElement("div");
+    modal.className = "dbr-modal";
+    let status = document.createElement("div");
+    status.className = "dbr-status";
+    let resultList = document.createElement("ol");
+    resultList.className = "dbr-results";
+    let closeBtn = document.createElement("button");
+    closeBtn.innerText = "Close";
+    closeBtn.addEventListener("click", () => {
+      this.removeModal();
+    });
+    modal.appendChild(status);
+    modal.appendChild(resultList);
+    modal.appendChild(closeBtn);
+    document.body.appendChild(modal);
+  },
+  removeModal: function(){
+    document.querySelector(".dbr-modal").remove();
+  },
+  decodeFromSrc: function(src, template) {
+    if (src) {
+      let img = document.createElement("img");
+      img.crossOrigin = "Anonymous";
+      img.src = src;  
+      img.onerror = function (e) {
+        DBRChromeExtension.showModal();
+        DBRChromeExtension.updateStatus("Unable to load the image");
+      }
+      img.onload = async function() {
+        await DBRChromeExtension.decode(img, template);
+        document.getElementById("dbr").remove();
+      }
     }
   }
 }
